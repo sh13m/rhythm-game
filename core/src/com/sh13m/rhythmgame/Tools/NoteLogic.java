4d15122daca7a5b3533558830f961b7ce79ad115
Sam S <66765667+sh13m@users.noreply.github.com>
2022-01-20 21:35:17 -0500

    added gameplay stats

--- core/src/com/sh13m/rhythmgame/Tools/NoteLogic.java
+++ core/src/com/sh13m/rhythmgame/Tools/NoteLogic.java
@@ -1,6 +1,7 @@
 package com.sh13m.rhythmgame.Tools;
 
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
 import com.sh13m.rhythmgame.Objects.Bar;
 import com.sh13m.rhythmgame.Objects.End;
 import com.sh13m.rhythmgame.Objects.Head;
@@ -9,11 +10,20 @@ import com.sh13m.rhythmgame.Screens.Gameplay;
 
 public class NoteLogic {
     public int COMBO;
+    public int MAX_COMBO;
     public String JUDGEMENT;
+    public int HEALTH;
+    public float ACCURACY;
+    public long SCORE;
+    private final Array<Float> accuracyIndex;
 
     public NoteLogic() {
         COMBO = 0;
         JUDGEMENT = "NONE";
+        HEALTH = 100;
+        ACCURACY = 100;
+        SCORE = 0;
+        accuracyIndex = new Array<>();
     }
 
     public void updateNotes(SongReader sr,
@@ -31,10 +41,20 @@ public class NoteLogic {
         for (int i=len; --i >= 0; ) {
             note = sr.activeTapNotes.get(i);
             note.update(receptor1, receptor2, receptor3, receptor4);
-            if (note.comboAdd) COMBO++;
-            if (note.comboBreak) COMBO = 0;
-            if (note.missed) JUDGEMENT = "MISS";
             if (note.isHit) getJudgement(note.getY());
+            if (note.missed) {
+                JUDGEMENT = "MISS";
+                updateAccuracy(0);
+            }
+            if (note.comboAdd) {
+                COMBO++;
+                if (COMBO > MAX_COMBO) MAX_COMBO = COMBO;
+            }
+            if (note.comboBreak) {
+                COMBO = 0;
+                HEALTH -= 5;
+                if (HEALTH < 0) HEALTH = 0;
+            }
             if (!note.alive) {
                 sr.activeTapNotes.removeIndex(i);
                 sr.tapNotePool.free(note);
@@ -62,12 +82,23 @@ public class NoteLogic {
 
             if (head.comboBreak) {
                 COMBO = 0;
-                if (head.gotJudgement) JUDGEMENT = "BAD";
-                else JUDGEMENT = "MISS";
+                if (head.gotJudgement) {
+                    JUDGEMENT = "BAD";
+                    updateAccuracy(60);
+                    HEALTH -= 2.5;
+                    if (HEALTH < 0) HEALTH = 0;
+                }
+                else {
+                    JUDGEMENT = "MISS";
+                    updateAccuracy(0);
+                    HEALTH -= 5;
+                    if (HEALTH < 0) HEALTH = 0;
+                }
                 head.comboBreak = false;
             }
             if (head.isHit) {
                 COMBO++;
+                if (COMBO > MAX_COMBO) MAX_COMBO = COMBO;
                 getJudgement(head.getY());
                 head.gotJudgement = true;
             }
@@ -87,14 +118,77 @@ public class NoteLogic {
     }
 
     private void getJudgement(float y) {
-        if (y < (float)Gameplay.R_HEIGHT - 54) JUDGEMENT = "BAD";
-        else if (y < (float)Gameplay.R_HEIGHT - 42) JUDGEMENT = "GOOD";
-        else if (y < (float)Gameplay.R_HEIGHT - 28) JUDGEMENT = "GREAT";
-        else if (y < (float)Gameplay.R_HEIGHT - 14) JUDGEMENT = "PERFECT";
-        else if (y < (float)Gameplay.R_HEIGHT + 14) JUDGEMENT = "MAX";
-        else if (y < (float)Gameplay.R_HEIGHT + 28) JUDGEMENT = "PERFECT";
-        else if (y < (float)Gameplay.R_HEIGHT + 42) JUDGEMENT = "GREAT";
-        else if (y < (float)Gameplay.R_HEIGHT + 54) JUDGEMENT = "GOOD";
-        else if (y < (float)Gameplay.R_HEIGHT + 64) JUDGEMENT = "BAD";
+        if (y < (float)Gameplay.R_HEIGHT - 54) {
+            JUDGEMENT = "BAD";
+            updateAccuracy(60);
+            SCORE += 600;
+            HEALTH += 1;
+            if (HEALTH > 100) HEALTH = 100;
+        }
+        else if (y < (float)Gameplay.R_HEIGHT - 42) {
+            JUDGEMENT = "GOOD";
+            updateAccuracy(70);
+            SCORE += 700;
+            HEALTH += 2;
+            if (HEALTH > 100) HEALTH = 100;
+        }
+        else if (y < (float)Gameplay.R_HEIGHT - 28) {
+            JUDGEMENT = "GREAT";
+            updateAccuracy(80);
+            SCORE += 800;
+            HEALTH += 3;
+            if (HEALTH > 100) HEALTH = 100;
+        }
+        else if (y < (float)Gameplay.R_HEIGHT - 14) {
+            JUDGEMENT = "PERFECT";
+            updateAccuracy(100);
+            SCORE += 900;
+            HEALTH += 5;
+            if (HEALTH > 100) HEALTH = 100;
+        }
+        else if (y < (float)Gameplay.R_HEIGHT + 14) {
+            JUDGEMENT = "MAX";
+            updateAccuracy(100);
+            SCORE += 1000;
+            HEALTH += 5;
+            if (HEALTH > 100) HEALTH = 100;
+        }
+        else if (y < (float)Gameplay.R_HEIGHT + 28) {
+            JUDGEMENT = "PERFECT";
+            updateAccuracy(100);
+            SCORE += 900;
+            HEALTH += 5;
+            if (HEALTH > 100) HEALTH = 100;
+        }
+        else if (y < (float)Gameplay.R_HEIGHT + 42) {
+            JUDGEMENT = "GREAT";
+            updateAccuracy(80);
+            SCORE += 800;
+            HEALTH += 3;
+            if (HEALTH > 100) HEALTH = 100;
+        }
+        else if (y < (float)Gameplay.R_HEIGHT + 54) {
+            JUDGEMENT = "GOOD";
+            updateAccuracy(70);
+            SCORE += 700;
+            HEALTH += 2;
+            if (HEALTH > 100) HEALTH = 100;
+        }
+        else if (y < (float)Gameplay.R_HEIGHT + 64) {
+            JUDGEMENT = "BAD";
+            updateAccuracy(60);
+            SCORE += 600;
+            HEALTH += 1;
+            if (HEALTH > 100) HEALTH = 100;
+        }
+    }
+
+    private void updateAccuracy(float acc) {
+        accuracyIndex.add(acc);
+        float sum = 0;
+        for (float num : accuracyIndex) {
+            sum += num;
+        }
+        ACCURACY = sum / accuracyIndex.size;
     }
 }
