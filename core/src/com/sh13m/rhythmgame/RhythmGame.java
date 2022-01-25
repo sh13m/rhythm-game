d40ac321ac40551d838ab9839577b763bc898372
Sam S <66765667+sh13m@users.noreply.github.com>
2022-01-25 12:49:23 -0500

    final commit

--- core/src/com/sh13m/rhythmgame/RhythmGame.java
+++ core/src/com/sh13m/rhythmgame/RhythmGame.java
@@ -34,7 +34,9 @@ public class RhythmGame extends Game {
 	public void create() {
 		displayMode = Gdx.graphics.getDisplayMode();
 		V_WIDTH = 480*displayMode.width/displayMode.height;
-		isFullScreen = false;
+		Gdx.graphics.setUndecorated(true);
+		Gdx.graphics.setWindowedMode(displayMode.width, displayMode.height);
+		isFullScreen = true;
 
 		batch = new SpriteBatch();
 		cam = new OrthographicCamera();
