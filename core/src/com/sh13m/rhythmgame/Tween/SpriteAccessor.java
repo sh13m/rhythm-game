419c422005e1537316d5b669edc226562b0cf990
Sam S <66765667+sh13m@users.noreply.github.com>
2022-01-24 22:14:18 -0500

    start making transitions

---
+++ core/src/com/sh13m/rhythmgame/Tween/SpriteAccessor.java
@@ -0,0 +1,15 @@
+package com.sh13m.rhythmgame.Tween;
+
+import aurelienribon.tweenengine.TweenAccessor;
+
+public class SpriteAccessor implements TweenAccessor {
+    @Override
+    public int getValues(Object target, int tweenType, float[] returnValues) {
+        return 0;
+    }
+
+    @Override
+    public void setValues(Object target, int tweenType, float[] newValues) {
+
+    }
+}
